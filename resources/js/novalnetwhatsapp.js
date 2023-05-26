b58a5ce2a07028d93c1ee3a73157c260c15593d2
abirajs <novalnetchennai@gmail.com>
2023-05-26 19:34:25 +0530

    Update novalnetwhatsapp.js

--- resources/js/novalnetwhatsapp.js
+++ resources/js/novalnetwhatsapp.js
@@ -1,3 +1,7 @@
 function myFunction() {
  window.location.href = "https://web.whatsapp.com/send?phone=8754754860&text=hi how can i help you";
 }
+
+$("#whatsappwidget").click(function(){
+    $(".whatsappcontainer").toggle();
+  });
