a653cac1eafbffef0fcd30fb681515dce7a01f35
abirajs <novalnetchennai@gmail.com>
2023-05-29 17:34:01 +0530

    Update novalnetwhatsapp.js

--- resources/js/novalnetwhatsapp.js
+++ resources/js/novalnetwhatsapp.js
@@ -1,19 +1,3 @@
-function myFunction() {
- window.location.href = "https://web.whatsapp.com/send?phone=8754754860&text=hi how can i help you";
-}
-
-function manager() {
- window.location.href = "https://web.whatsapp.com/send?phone=9488732051&text=hi how can i help you";
-}
-
-function leader() {
- window.location.href = "https://web.whatsapp.com/send?phone=9488732052&text=hi how can i help you";
-}
-
-function employee() {
- window.location.href = "https://web.whatsapp.com/send?phone=8754754860&text=hi how can i help you";
-}
-
 $(document).ready(function(){
    $(".whatsappwidget").click(function(){
     $(".whatsappcontainer").toggle();
@@ -24,3 +8,9 @@ $(document).ready(function(){
   });
  
 });
+
+function whatsapp(id) { 
+	var test =  $('.member'+id).attr("phone");
+	alert(test);  
+	//~ window.location.href = "https://web.whatsapp.com/send?phone=8754754860&text=hi how can i help you";
+}
