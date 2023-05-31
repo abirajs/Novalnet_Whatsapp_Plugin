ca579c6040b3accff3e961235e7614ac9522b556
abirajs <novalnetchennai@gmail.com>
2023-05-31 11:51:43 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -16,4 +16,13 @@ class NovalnetServiceProvider extends ServiceProvider
 	{
 
 	}
+	/**
+	* Boot additional services for the payment method
+	 *
+	*/
+	public function boot()
+	{
+	     // Set the Novalnet Assistant
+	     pluginApp(WizardContainerContract::class)->register('template-novalnet-assistant', NovalnetAssistant::class);
+	}
 }
