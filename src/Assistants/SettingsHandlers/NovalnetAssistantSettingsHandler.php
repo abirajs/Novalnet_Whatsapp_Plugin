4ba88b69737e2bbdab46aef5d5d1f283e6be0e0b
abirajs <novalnetchennai@gmail.com>
2023-05-31 17:33:41 +0530

    Update NovalnetAssistantSettingsHandler.php

--- src/Assistants/SettingsHandlers/NovalnetAssistantSettingsHandler.php
+++ src/Assistants/SettingsHandlers/NovalnetAssistantSettingsHandler.php
@@ -9,7 +9,6 @@
  */
 namespace NovalnetWhatsapp\Assistants\SettingsHandlers;
 
-use NovalnetWhatsapp\Helper\PaymentHelper;
 use NovalnetWhatsapp\Services\SettingsService;
 use Plenty\Modules\Plugin\PluginSet\Contracts\PluginSetRepositoryContract;
 use Plenty\Modules\Wizard\Contracts\WizardSettingsHandler;
@@ -25,76 +24,27 @@ class NovalnetAssistantSettingsHandler implements WizardSettingsHandler
     {
         /** @var PluginSetRepositoryContract $pluginSetRepo */
         $pluginSetRepo = pluginApp(PluginSetRepositoryContract::class);
-        /** @var PaymentHelper $paymentHelper */
-        $paymentHelper = pluginApp(PaymentHelper::class);
         $clientId = $postData['data']['clientId'];
         $pluginSetId = $pluginSetRepo->getCurrentPluginSetId();
         $data = $postData['data'];
-        // Novalnet global and webhook configuration values
+        // Novalnet global and webhook configurations values
         $novalnetSettings=[
-            'novalnet_public_key'       =>  $data['novalnetPublicKey'] ?? '',
-            'novalnet_private_key'      =>  $data['novalnetAccessKey'] ?? '',
-            'novalnet_tariff_id'        =>  $data['novalnetTariffId'] ?? '',
-            'novalnet_client_key'       =>  $data['novalnetClientKey'] ?? '',
-            'novalnet_order_creation'   =>  $data['novalnetOrderCreation'] ?? '',
-            'novalnet_webhook_testmode' =>  $data['novalnetWebhookTestMode'] ?? '',
-            'novalnet_webhook_email_to' =>  $data['novalnetWebhookEmailTo'] ?? '',
+            'nn_whatsapp_enable_chat'       =>  $data['enableChat'] ?? '',
+            'nn_whatsapp_chat_heading'      =>  $data['chatHeading'] ?? '',
+            'nn_whatsapp_chat_description'  =>  $data['chatDescription'] ?? '',
+            'nn_whatsapp_mobile_number'     =>  $data['mobileNumber'] ?? '',
+            'nn_whatsapp_account_name'      =>  $data['accountName'] ?? '',
+            'nn_whatsapp_account_role'      =>  $data['accountRole'] ?? '',
+            'nn_whatsapp_profile_logo'      =>  $data['profileLogo'] ?? '',
+            'nn_whatsapp_open_new_tab'  	=>  $data['openNewTab'] ?? '',
+            'nn_whatsapp_url_desktop'       =>  $data['URLforDesktop'] ?? '',
+            'nn_whatsapp_url_mobile'        =>  $data['URLforMobile'] ?? '',
+            'nn_whatsapp_mobile_theme'      =>  $data['mobileTheme'] ?? '',
+            'nn_whatsapp_mobile_shape'      =>  $data['mobileShape'] ?? '',
+            'nn_whatsapp_desktop_theme'     =>  $data['desktopTheme'] ?? '',
+            'nn_whatsapp_desktop_shape'     =>  $data['desktopShape'] ?? '',
         ];
-        // Payment method common configuration values
-        foreach($paymentHelper->getPaymentMethodsKey() as $paymentMethodKey) {
-            $paymentKey=str_replace('_','',ucwords(strtolower($paymentMethodKey),'_'));
-            $paymentKey[0] = strtolower($paymentKey[0]);
-            $paymentMethodKey = strtolower($paymentMethodKey);
-            $novalnetSettings[$paymentMethodKey]['payment_active']               = $data[$paymentKey . 'PaymentActive'] ?? '';
-            $novalnetSettings[$paymentMethodKey]['test_mode']                    = $data[$paymentKey . 'TestMode'] ?? '';
-            $novalnetSettings[$paymentMethodKey]['payment_logo']                 = $data[$paymentKey . 'PaymentLogo'] ?? '';
-            $novalnetSettings[$paymentMethodKey]['minimum_order_amount']         = $data[$paymentKey . 'MinimumOrderAmount'] ?? '';
-            $novalnetSettings[$paymentMethodKey]['maximum_order_amount']         = $data[$paymentKey . 'MaximumOrderAmount'] ?? '';
-            $novalnetSettings[$paymentMethodKey]['allowed_country']              = $data[$paymentKey . 'AllowedCountry'] ?? '';
 
-            switch ($paymentMethodKey) {
-                case 'novalnet_cc':
-                    $novalnetSettings[$paymentMethodKey]['enforce']              = $data[$paymentKey . 'Enforce'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['inline_form']          = $data[$paymentKey . 'InlineForm'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['payment_action']       = $data[$paymentKey . 'PaymentAction'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['onhold_amount']        = $data[$paymentKey . 'OnHold'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['standard_style_label'] = $data[$paymentKey . 'StandardStyleLabel'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['standard_style_field'] = $data[$paymentKey . 'StandardStyleField'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['standard_style_css']   = $data[$paymentKey . 'StandardStyleCss'] ?? '';
-                    break;
-                case 'novalnet_invoice':
-                case 'novalnet_sepa':
-                    $novalnetSettings[$paymentMethodKey]['due_date']             = $data[$paymentKey . 'Duedate'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['payment_action']       = $data[$paymentKey . 'PaymentAction'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['onhold_amount']        = $data[$paymentKey . 'OnHold'] ?? '';
-                    break;
-                case 'novalnet_prepayment':
-                case 'novalnet_cashpayment':
-                    $novalnetSettings[$paymentMethodKey]['due_date']             = $data[$paymentKey . 'Duedate'] ?? '';
-                    break;
-                case 'novalnet_guaranteed_invoice':
-                case 'novalnet_guaranteed_sepa':
-                    $novalnetSettings[$paymentMethodKey]['force']                = $data[$paymentKey . 'force'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['allow_b2b_customer']   = $data[$paymentKey . 'allowB2bCustomer'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['payment_action']       = $data[$paymentKey . 'PaymentAction'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['onhold_amount']        = $data[$paymentKey . 'OnHold'] ?? '';
-                    break;
-                case 'novalnet_paypal':
-                case 'novalnet_applepay':
-                    $novalnetSettings[$paymentMethodKey]['payment_action']       = $data[$paymentKey . 'PaymentAction'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['onhold_amount']        = $data[$paymentKey . 'OnHold'] ?? '';
-                    break;
-                case 'novalnet_googlepay':
-                    $novalnetSettings[$paymentMethodKey]['merchant_id']          = $data[$paymentKey . 'MerchantId'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['business_name']        = $data[$paymentKey . 'BusinessName'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['enforce']              = $data[$paymentKey . 'Enforce'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['payment_action']       = $data[$paymentKey . 'PaymentAction'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['onhold_amount']        = $data[$paymentKey . 'OnHold'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['button_type']          = $data[$paymentKey . 'ButtonType'] ?? '';
-                    $novalnetSettings[$paymentMethodKey]['button_height']        = $data[$paymentKey . 'ButtonHeight'] ?? '';
-                    break;
-            }
-        }
         /** @var SettingsService $settingsService */
         $settingsService=pluginApp(SettingsService::class);
         $settingsService->updateSettings($novalnetSettings, $clientId, $pluginSetId);
