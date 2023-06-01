17e3cf4c9f1673864d64548705b3a71bff873c5a
abirajs <novalnetchennai@gmail.com>
2023-06-01 17:59:17 +0530

    Update WhatsappHelper.php

--- src/Helper/WhatsappHelper.php
+++ src/Helper/WhatsappHelper.php
@@ -33,7 +33,6 @@ class WhatsappHelper
     
     public function isMobileDev(){       
         if(!empty($_SERVER['HTTP_USER_AGENT'])){
-            alert('2');
            $user_ag = $_SERVER['HTTP_USER_AGENT'];
            if(preg_match('/(Mobile|Android|Tablet|GoBrowser|[0-9]x[0-9]*|uZardWeb\/|Mini|Doris\/|Skyfire\/|iPhone|Fennec\/|Maemo|Iris\/|CLDC\-|Mobi\/)/uis',$user_ag)){
               return true;
