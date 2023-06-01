86118459d33b42bb1c24c939d351343ed6832acb
abirajs <novalnetchennai@gmail.com>
2023-06-01 14:20:15 +0530

    Update NovalnetWhatsappProvider.php

--- src/Providers/DataProvider/NovalnetWhatsappProvider.php
+++ src/Providers/DataProvider/NovalnetWhatsappProvider.php
@@ -16,12 +16,34 @@ class NovalnetWhatsappProvider extends ServiceProvider
     public function call(Twig $twig):string
     {
        	$settingsService        = pluginApp(SettingsService::class);
-        $accountName 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_account_name');
+	$enableChat 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_enable_chat');
+	$chatHeading 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_chat_heading');
+	$chatDescription	= $settingsService->getPaymentSettingsValue('nn_whatsapp_chat_description');
+	$sccountName 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_account_name');
+	$mobileNumber 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_mobile_number');
+	$profileLogo 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_profile_logo');
+	$openNewTab 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_open_new_tab');
+	$desktopURL 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_url_desktop');
+	$mobileURL 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_url_mobile');
+	$mobileTheme 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_mobile_theme');
+	$mobileShape 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_mobile_shape');
+	$desktopTheme 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_desktop_theme');
+	$desktopShape 		= $settingsService->getPaymentSettingsValue('nn_whatsapp_desktop_shape');
 	
 	$this->getLogger(__METHOD__)->error('AccountName', $accountName);
         return $twig->render('NovalnetWhatsapp::NovalnetWhatsappDataProvider',
 			     [
-				 'accountName' =>  $accountName,		
+				'accountName' 		=>  $accountName,		
+				'chatHeading' 		=>  $chatHeading,		
+				'chatDescription' 	=>  $chatDescription,		
+				'openNewTab' 		=>  $openNewTab,		
+				'desktopURL' 		=>  $desktopURL,		
+				'mobileURL' 		=>  $mobileURL,		
+				'mobileTheme' 		=>  $mobileTheme,		
+				'mobileShape' 		=>  $mobileShape,		
+				'desktopTheme' 		=>  $desktopTheme,		
+				'desktopShape' 		=>  $desktopShape,		
+				     
 			     ]);
     }
 }
