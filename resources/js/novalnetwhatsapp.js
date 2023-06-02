efee9b0e79396aabbcadc7463695675c966d6540
abirajs <novalnetchennai@gmail.com>
2023-06-02 15:23:09 +0530

    Update novalnetwhatsapp.js

--- resources/js/novalnetwhatsapp.js
+++ resources/js/novalnetwhatsapp.js
@@ -13,34 +13,29 @@ function whatsapp(id) {
     var apiEndPoint = 'https://';
     var phone   =  $('.member'+id).attr("phone");
     var message =  'Hi how can i help you..'; 
-alert(jQuery('#nn_whatsapp_is_mobile').val());
+	
     // Find the mobile devices and redirect the api endpoint url
     if(jQuery('#nn_whatsapp_is_mobile').val() == 'true') {
 		if(jQuery('#nn_whatsapp_mobile_URL').val() == 'web') {
 			apiEndPoint = apiEndPoint+'web.whatsapp.com/send?phone=' + phone + '&text=' + (message);
-			alert(apiEndPoint);
 		} else if(jQuery('#nn_whatsapp_mobile_URL').val() == 'api') {
 			apiEndPoint = apiEndPoint+'api.whatsapp.com/send?phone=' + phone + '&text=' + (message);
-			alert(apiEndPoint);
 		} else if(jQuery('#nn_whatsapp_mobile_URL').val() == 'universal') {
 			apiEndPoint = apiEndPoint+'wa.me/' + phone + '?text=' + (message);
-			alert(apiEndPoint);
 		}		
 	} 
-    alert(jQuery('#nn_whatsapp_desktop_URL').val());
+
     // Find the desktop devices and redirect the api endpoint url
     if(jQuery('#nn_whatsapp_is_mobile').val() == 'false') {
 		if(jQuery('#nn_whatsapp_desktop_URL').val() == 'web') {
 			apiEndPoint = apiEndPoint+'web.whatsapp.com/send?phone=' + phone + '&text=' + (message);
-			alert(apiEndPoint);
 		} else if(jQuery('#nn_whatsapp_desktop_URL').val() == 'api') {
 			apiEndPoint = apiEndPoint+'api.whatsapp.com/send?phone=' + phone + '&text=' + (message);
-			alert(apiEndPoint);
 		} else if(jQuery('#nn_whatsapp_desktop_URL').val() == 'universal') {
 			apiEndPoint = apiEndPoint+'wa.me/' + phone + '?text=' + (message);
-			alert(apiEndPoint);
 		}		
 	}  
+	
     // set the new tab open redirect page
     if(jQuery('#nn_whatsapp_open_new_tab').val() == 1) {
 	window.open(apiEndPoint);
