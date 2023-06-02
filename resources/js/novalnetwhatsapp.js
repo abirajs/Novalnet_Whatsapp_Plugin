db9320cd980e589272d9b3107db2ebff76bfa780
abirajs <novalnetchennai@gmail.com>
2023-06-02 09:26:09 +0530

    Update novalnetwhatsapp.js

--- resources/js/novalnetwhatsapp.js
+++ resources/js/novalnetwhatsapp.js
@@ -43,7 +43,7 @@ alert(jQuery('#nn_whatsapp_is_mobile').val());
 	} 
   alert(apiEndPoint);  
     // set the new tab open redirect page
-    if(jQuery('#nn_whatsapp_open_new_tab').val() == 'true') {
+    if(jQuery('#nn_whatsapp_open_new_tab').val() == 1) {
 	window.open(apiEndPoint);
     } else {
 	window.location.href = apiEndPoint;
