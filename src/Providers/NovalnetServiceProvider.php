62a867d72c155796a28118660abb96fce97b4753
abirajs <novalnetchennai@gmail.com>
2023-06-02 15:41:16 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -5,18 +5,9 @@ namespace NovalnetWhatsapp\Providers;
 use NovalnetWhatsapp\Assistants\NovalnetAssistant;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Modules\Wizard\Contracts\WizardContainerContract;
-use Plenty\Plugin\Log\Loggable;
 
 class NovalnetServiceProvider extends ServiceProvider
 {
-	use Loggable;
-	/**
-	 * Register the service provider.
-	 */
-	public function register() 
-	{
-
-	}
 	/**
 	* Boot additional services for the payment method
 	 *
